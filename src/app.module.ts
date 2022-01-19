7f8561461711b54f9cafaf7a94a33501cbc072cd
airman5573 <rpf5573@kakao.com>
2022-01-19 13:51:24 +0900

    :bug: fix(env): 환경변수를 불러온다

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(),
+    AuthModule,
+  ],
 })
 export class AppModule {}
