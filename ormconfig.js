aa9f7c483e55aeec510da887de58a9f92152a461
airman5573 <rpf5573@kakao.com>
2022-01-21 22:38:27 +0900

    :bug: fix(migration): 테이블로 생성될 Entity를 지정한다
    이렇게 개별적으로 지정하지 않으면, 테이블로 생성될 필요 없는 Entity까지 테이블로 만들어지기 때문이다.

--- ormconfig.js
+++ ormconfig.js
@@ -5,7 +5,12 @@ module.exports = {
   username: process.env.DB_USERNAME || 'rpf5573',
   password: process.env.DB_PASSWORD || 'thoumas138',
   database: process.env.DB_DATABASE || 'discovery',
-  entities: ['dist/**/*.entity.js'],
+  entities: [
+    'dist/**/puzzle.entity.js',
+    'dist/**/team-password.entity.js',
+    'dist/**/team-point.entity.js',
+    'dist/**/timer.entity.js',
+  ],
   synchronize: false,
   migrations: ['dist/migration/*{.ts,.js}'],
   cli: { migrationsDir: 'dist/migration' },
