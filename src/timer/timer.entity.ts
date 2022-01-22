579254fc2307806bf4e36ab391818f2dd25b44a8
airman5573 <rpf5573@kakao.com>
2022-01-22 14:51:43 +0900

    :bug: fix(timer.entity): 불필요한 프로퍼티를 삭제한다

--- src/timer/timer.entity.ts
+++ src/timer/timer.entity.ts
@@ -1,13 +1,10 @@
 import { LocalDateTime } from '@js-joda/core';
 import { TeamBaseEntity } from 'src/entities/team.base.entity';
 import { LocalDateTimeTransformer } from 'src/utils/local-datetime-transformer';
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 
 @Entity({ name: 'timers' })
 export class TimerEntity extends TeamBaseEntity {
-  @PrimaryColumn()
-  team: number;
-
   @Column({
     name: 'start_time',
     type: 'datetime',
