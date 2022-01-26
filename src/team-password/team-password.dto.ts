b815190115760104a1ed039e4be8857612e0aa6c
airman5573 <rpf5573@kakao.com>
2022-01-26 13:54:44 +0900

    :recycle: refactor(team-password.dto): 타입을 변환한다
    string이 들어와도 알아서 number로 바뀌도록 변환 설정을 한다

--- src/team-password/team-password.dto.ts
+++ src/team-password/team-password.dto.ts
@@ -2,6 +2,7 @@ import { Type } from 'class-transformer';
 import { IsArray, IsNumber, IsString } from 'class-validator';
 
 export class TeamPasswordDto {
+  @Type(() => Number)
   @IsNumber()
   team: number;
 
