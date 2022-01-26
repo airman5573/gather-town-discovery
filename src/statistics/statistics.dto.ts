a1c396a69b6e26baebc0fab40ca4f0e4f4891c46
airman5573 <rpf5573@kakao.com>
2022-01-26 10:27:55 +0900

    :recycle: refactor(statistics): number타입으로 변환한다
    string이 들어올 수도 있기 때문에, 변환이 필요하다

--- src/statistics/statistics.dto.ts
+++ src/statistics/statistics.dto.ts
@@ -1,36 +1,48 @@
+import { Type } from 'class-transformer';
 import { IsInt, IsPositive } from 'class-validator';
 
 export class TeamStatisticsDto {
+  @Type(() => Number)
   @IsPositive()
   team: number;
 
+  @Type(() => Number)
   @IsPositive()
   usable: number;
 
+  @Type(() => Number)
   @IsInt()
   timer: number;
 
+  @Type(() => Number)
   @IsPositive()
   boxOpen: number;
 
+  @Type(() => Number)
   @IsPositive()
   sentenceDecryption: number;
 
+  @Type(() => Number)
   @IsPositive()
   bingo: number;
 
+  @Type(() => Number)
   @IsPositive()
   openEmptyBoxCount: number;
 
+  @Type(() => Number)
   @IsPositive()
   openLetterBoxCount: number;
 
+  @Type(() => Number)
   @IsPositive()
   percentageOfBoxOpen: number;
 
+  @Type(() => Number)
   @IsPositive()
   sumOfPoint: number;
 
+  @Type(() => Number)
   @IsPositive()
   contributionRank: number;
 }
