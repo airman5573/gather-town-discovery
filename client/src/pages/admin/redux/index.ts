349b9496d60d8e6cfb106a1cd17c001db111196d
airman5573 <rpf5573@kakao.com>
2022-02-05 11:12:54 +0900

    :recycle: refactor(slice): 사용하지 않는 slice를 삭제한다

--- client/src/pages/admin/redux/index.ts
+++ client/src/pages/admin/redux/index.ts
@@ -5,8 +5,6 @@ import teamPasswordApi from './api/team-password.api';
 import timerApi from './api/timer.api';
 import teamPointApi from './api/team-point.api';
 import modalControlReducer from './features/modal-control.slice';
-import teamPasswordReducer from './features/team-password.slice';
-import timerReducer from './features/timers.slice';
 import pointTableApi from './api/point-table.api';
 import resetApi from './api/reset.api';
 import statisticsApi from './api/statistics.api';
@@ -14,8 +12,6 @@ import statisticsApi from './api/statistics.api';
 export const store = configureStore({
   reducer: {
     modalControl: modalControlReducer,
-    teamPasswords: teamPasswordReducer,
-    timers: timerReducer,
     [teamPasswordApi.reducerPath]: teamPasswordApi.reducer,
     [timerApi.reducerPath]: timerApi.reducer,
     [optionApi.reducerPath]: optionApi.reducer,
