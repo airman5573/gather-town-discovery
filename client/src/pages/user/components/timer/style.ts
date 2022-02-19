f4fc6e961af6f7eb2b13c336c37e639d6e7780eb
airman5573 <rpf5573@kakao.com>
2022-02-19 22:37:45 +0900

    :lipstick: style(timer): 타이머의 위치를 변경한다

--- client/src/pages/user/components/timer/style.ts
+++ client/src/pages/user/components/timer/style.ts
@@ -2,8 +2,9 @@ import { css } from '@emotion/react';
 
 export const timerContainerStyle = css`
   position: fixed;
-  top: 20px;
-  right: 20px;
+  bottom: 20px;
+  left: 20px;
+  z-index: 10;
   background: white;
   padding: 10px;
   font-size: 40px;
